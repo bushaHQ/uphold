b32bf80f2e6bc2f1115605363da28b048209d479
Codefarmer <abadasamuelosp@gmail.com>
2026-03-10 13:15:26 +0100

    feat: uphold library

---
+++ lib/uphold_payment_widget.dart
@@ -0,0 +1,8 @@
+/// Flutter package for the Uphold Payment Widget.
+///
+/// Wraps the Uphold `@uphold/enterprise-payment-widget-web-sdk` in a WebView
+/// and bridges events back to Dart through a JavaScriptChannel.
+library;
+
+export 'src/models.dart';
+export 'src/uphold_payment_widget.dart';
